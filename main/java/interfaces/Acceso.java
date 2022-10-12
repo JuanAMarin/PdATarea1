5dd7b5fc9e7d36f5ae433b614e3da2e194b990bf
_ Jonatan _ <PrincipalJEMS@gmail.com>
2022-10-11 23:05:29 -0300

    aparentemente modificar usuario ahora si está terminado

--- main/java/interfaces/Acceso.java
+++ main/java/interfaces/Acceso.java
@@ -1,5 +1,7 @@
 package interfaces;
 
+import datatypes.DtActividadDep;
+import datatypes.DtInstitucionDep;
 import logica.Profesor;
 import logica.Socio;
 
@@ -8,6 +10,9 @@ public class Acceso {
 	private static Acceso instancia;
 	private Profesor p;
 	private Socio s;
+	private DtActividadDep actividad;
+	private DtInstitucionDep institucion;
+	private boolean modal;
 	
 	public static Acceso getInstancia() {
         if (instancia == null) {
@@ -31,10 +36,36 @@ public class Acceso {
 	public void setS(Socio s) {
 		this.s = s;
 	}
+	
+	public DtActividadDep getActividad() {
+		return actividad;
+	}
+
+	public void setActividad(DtActividadDep actividad) {
+		this.actividad = actividad;
+	}
+
+	public boolean isModal() {
+		return modal;
+	}
+
+	public void setModal(boolean modal) {
+		this.modal = modal;
+	}
+
+	public DtInstitucionDep getInstitucion() {
+		return institucion;
+	}
+
+	public void setInstitucion(DtInstitucionDep institucion) {
+		this.institucion = institucion;
+	}
 
 	public void killInstancia() {
 		instancia = null;
 		p = null;
 		s = null;
+		actividad = null;
+		modal = false;
 	}
 }
